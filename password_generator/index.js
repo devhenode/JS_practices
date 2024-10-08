b5d5b6d9bfb14fca8284ed0812fc973891a3ece6
devhenode <devodehenry.2000@gmail.com>
2024-10-08 22:15:01 +0100

    added copy to clipboard and choice of length

--- password_generator/index.js
+++ password_generator/index.js
@@ -8,23 +8,65 @@ let btnEl = document.getElementById("btn-El");
 let btnOne = document.getElementById("btn-1");
 let btnTwo = document.getElementById("btn-2");
 
-btnEl.addEventListener("click", function() {
-  console.log("Hello world");                    
+var length_Of_Password;
 
+const selected = () => {
+  let getOpt = document.getElementById("selector");
+  let getIndex = getOpt.selectedIndex;
+  length_Of_Password = getOpt.options[getIndex].text;
+  
+  console.log("You just selected " + length_Of_Password);
+}
+
+btnEl.addEventListener("click", function() {
+  console.log("Hello world");
   let result = '';
   let count = '';
   
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < length_Of_Password; i++) {
     result += characters[Math.floor(Math.random() * characters.length)];
     count += characters[Math.floor(Math.random() * characters.length)];
-    console.log(result);                    
+    // console.log(result);     
+    // console.log(result)
+               
   }
 
   btnOne.textContent = result;
   btnTwo.textContent = count;
 
+
 });
 
+const copyText = async () => {
+  let hashText = document.getElementById('btn-1').innerHTML;
+  try {
+    await navigator.clipboard.writeText(hashText);
+    alert(`${hashText} copied to clipboard`)
+  } catch (err) {
+    console.error("Failed to copy: ", err)
+  }
+}
+
+
+
+
+// function copyText() {
+//   let copiedText = document.getElementById("btn-1");
+//   let copiedTextTwo = document.getElementById("btn-2");
+
+//   copiedText.select();
+//   copiedText.setSelectionRange(0, 99999);
+
+//   copiedTextTwo.select();
+//   copiedTextTwo.setSelectionRange(0, 99999);
+
+//   navigator.clipboard.writeText(copiedText.value);
+//   navigator.clipboard.writeText(copiedTextTwo.value);
+
+//   alert("Copied the text:");
+// }
+
+// copyText();
 // btnEl.addEventListener("click", generateRandomString(10));
 
 
